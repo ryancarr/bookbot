7ba825c4ada2ea7a6b66263156e9e7220d4ca071
ryancarr <rycarr@gmail.com>
2024-09-30 21:50:19 -0700

    Updated code for Print a Report portion of the assignment

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def count_words(text: str) -> int:
     """
     return len(text.split())
 
-def count_characters(text: str) -> dict[str, int]:
+def count_characters(text: str, symbols: bool =True) -> dict[str, int]:
     """
     This function counts the number of individual characters in a block of text. All characters
     are treated as lowercase. Instructions weren't clear how to handle numbers, symbols or
@@ -18,24 +18,59 @@ def count_characters(text: str) -> dict[str, int]:
     
     Args:
         text (str): The input block of text that you need a character count
+        symbols (bool): Optional argument that determine if nonalpha characters are included.
         
     Returns:
-        dict (str:int): A dictionary where characters are the keys and values are how many times the
-        letter is seen.
+        dict (str, int): A dictionary where characters are the keys and values are how 
+        many times the letter is seen.
     """
     output = {}
     
     for letter in text.lower():
-        # Update the value of the letter. If a letter hasn't been seen yet default to 0 and add 1
+        # If we are excluding symbols and the current letter is not in the alphabet skip
+        # current letter
+        if not symbols and not letter.isalpha():
+            continue
+        
+        # Update the value of the character. If a character hasn't been seen yet 
+        # default to 0 and add 1
         output[letter] = output.get(letter, 0) + 1
     
     return output
 
-try:
-    with open('books/frankenstein.txt') as fh:
-        text = fh.read()
+def sort_char_dictionary(character_dict: dict[str, int]) -> dict[str, int]:
+    """
+    Takes an unsorted dictionary of characters, removing non alphanumeric characters and
+    sort based on character frequency.
     
-    print(count_words(text))
-    print(count_characters(text))
+    Args:
+        dict (str, int): A dictionary that is unsorted made of string, int pairs.
+        
+    
+    Returns:
+        dict (str, int): A sorted dictionary, sorted in reverse value order.
+    """
+    return dict(sorted(character_dict.items(), key=lambda item: item[1], reverse=True))
+
+
+filename = 'books/frankenstein.txt'
+
+try:
+    with open(filename) as fh:
+        text = fh.read()
+
+    word_count = count_words(text)
+    character_dict = count_characters(text, False)
 except FileNotFoundError:
-    print('''Error: The file frankenstein.txt was not found.''')
+    print(f"Error: The file {filename} was not found.")
+    exit()
+
+output = sort_char_dictionary(character_dict)
+
+print(f"--- Begin report of {filename} ---")
+print(f"{word_count} words found in the document\n")
+
+for k, v in output.items():
+    print(f"The '{k}' character was found {v} times")
+
+print("--- End report ---")
